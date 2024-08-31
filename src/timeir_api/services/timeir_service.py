3c0cdf2ec1ce24558c1fc8dbda84373c5eeab16c
mostafa2k <m2kappswindows8.1@gmail.com>
2024-08-31 08:48:13 +0330

    feat: add async function to fetch `random quote` from time.ir

--- src/timeir_api/services/timeir_service.py
+++ src/timeir_api/services/timeir_service.py
@@ -17,3 +17,9 @@ async def get_current_date():
     days = [d.text for d in scraper.select("div.dateText span")][:3]
     date_time = {months[num]: days[num] for num in range(len(days))}
     return date_time
+
+
+async def get_random_quote():
+    scraper = await get_content()
+    quote_text = scraper.find("div", class_="randomQuote").find("span").text.strip()
+    return quote_text
