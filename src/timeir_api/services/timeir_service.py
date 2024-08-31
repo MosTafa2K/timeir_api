ec28099de91248aaa417054102806f82b8e1f074
mostafa2k <m2kappswindows8.1@gmail.com>
2024-08-31 07:47:28 +0330

    feat: Add Services to parse and extract `date` data from time.ir
    - `get_content()`: async function to fetch and parse HTML content
    
    - `date_parser()`:  async function to parse and extract `date` information from HTML content

---
+++ src/timeir_api/services/timeir_service.py
@@ -0,0 +1,19 @@
+from bs4 import BeautifulSoup
+import httpx
+
+URL = "https://www.time.ir/"
+
+
+async def get_content():
+    async with httpx.AsyncClient() as client:
+        response = await client.get(URL)
+        soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+
+async def date_parser():
+    scraper = await get_content()
+    months = [m.text for m in scraper.select("div.dateType span")][:3]
+    days = [d.text for d in scraper.select("div.dateText span")][:3]
+    date_time = {months[num]: days[num] for num in range(len(days))}
+    return date_time
