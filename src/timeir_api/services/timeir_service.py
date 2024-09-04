1b770e5d24e7d64315ae2da9fd5548ab90629e73
mostafa2k <m2kappswindows8.1@gmail.com>
2024-09-04 12:19:52 +0330

    perf: improve performance by manually defining months in get_current_date
    Avoids additional scraping by manually setting the months list
    (solar, gregorian, lunar) to reduce unnecessary HTML parsing and enhance performance.

--- src/timeir_api/services/timeir_service.py
+++ src/timeir_api/services/timeir_service.py
@@ -19,7 +19,7 @@ async def get_content():
 
 async def get_current_date():
     scraper = await get_content()
-    months = [m.text for m in scraper.select("div.dateType span")][:3]
+    months = ["solar", "gregorian", "lunar"]
     days = [d.text for d in scraper.select("div.dateText span")][:3]
     date_time = {months[num]: days[num] for num in range(len(days))}
     return date_time
