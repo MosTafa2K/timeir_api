bbc067be9cee92a83f75062bb3bc9e1c1ea95ec6
mostafa2k <m2kappswindows8.1@gmail.com>
2024-09-05 21:37:11 +0330

    Rename test functions for improved clarity

--- tests/test_timeir_routers.py
+++ tests/test_timeir_routers.py
@@ -4,19 +4,19 @@ from src.timeir_api.main import app
 client: TestClient = TestClient(app)
 
 
-def test_root():
+def test_root_route():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Welcome To Unofficial Timeir API✨"}
 
 
-def test_current_date():
+def test_current_date_route():
     response = client.get("/api/v1/date/current")
     assert response.status_code == 200
     assert response is not None
 
 
-def test_random_quote():
+def test_random_quote_route():
     response = client.get("/api/v1/quote")
     assert response.status_code == 200
     assert response is not None
